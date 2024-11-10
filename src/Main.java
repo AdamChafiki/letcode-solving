c1415cfa2295c2597c7c5cfbebd73a4ee178b69f
Chafiki <adamchafiki3@gmail.com>
2024-11-10 11:54:03 +0100

    init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//        inner class / jvm .... / upcastin class / Bufferstring /interface & abstract / enum class / annotation
+//        / anonymous class / THREAD , RUNNABLE / collection API
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/solving/rotateString/Solution.java
@@ -0,0 +1,14 @@
+package solving.rotateString;
+
+public class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        String doubled = s + s;
+
+        return doubled.contains(goal);
+    }
+}
+
